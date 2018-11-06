267dbe4270a4ae0ab37c44e4d56e0b9b95ec120d
remy <remy.auricoste@gmail.com>
2018-11-06 15:05:33 +0100

    Router : add CORS

--- src/main/scala/fr/drysoft/pocAkka/http/Router.scala
+++ src/main/scala/fr/drysoft/pocAkka/http/Router.scala
@@ -1,6 +1,8 @@
 package fr.drysoft.pocAkka.http
 
+import akka.http.javadsl.model.headers.AccessControlAllowOrigin
 import akka.http.scaladsl.marshalling.sse.EventStreamMarshalling._
+import akka.http.scaladsl.model.headers.{HttpOrigin, HttpOriginRange}
 import akka.http.scaladsl.model.sse.ServerSentEvent
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
@@ -14,23 +16,27 @@ case class Router @Inject() (
     appConfig: AppConfig)(
         implicit materializer: ActorMaterializer) {
 
-  val route: Route =
-    path("filter") {
-      post {
-        parameter("min".as[Int], "max".as[Int]) { (min, max) =>
-          eventController.filter(min, max)
-          complete(s"received filter request from $min to $max")
-        }
-      }
-    } ~
-      path("events") {
-        get {
-          complete {
-            eventController.broadcastSource
-              .map(number => s"incoming number $number")
-              .map(ServerSentEvent(_))
-              .keepAlive(appConfig.sseHeartbeatInterval, () => ServerSentEvent.heartbeat)
+  lazy val route: Route =
+    respondWithHeaders(
+      AccessControlAllowOrigin.create(HttpOriginRange(HttpOrigin("http://localhost:3000")))
+    ) {
+      path("filter") {
+        post {
+          parameter("min".as[Int], "max".as[Int]) { (min, max) =>
+            eventController.filter(min, max)
+            complete(s"received filter request from $min to $max")
           }
         }
-      }
+      } ~
+        path("events") {
+          get {
+            complete {
+              eventController.broadcastSource
+                .map(number => number.toString)
+                .map(ServerSentEvent(_))
+                .keepAlive(appConfig.sseHeartbeatInterval, () => ServerSentEvent.heartbeat)
+            }
+          }
+        }
+    }
 }
